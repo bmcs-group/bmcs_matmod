42a02be2c8dcd57a39f9dbf8130394f0c897390f
rch <rostislav.chudoba@rwth-aachen.de>
2022-02-17 18:44:44 +0100

    symbolic

--- bmcs_matmod/__init__.py
+++ bmcs_matmod/__init__.py
@@ -2,5 +2,3 @@
 from bmcs_matmod.version import __version__
 from .ms1 import MS1
 from .ms1 import MS13D
-from .ms1 import MS1_damage_jir
-from .ms1 import MS13D_damage_jir
--- bmcs_matmod/ms1/__init__.py
+++ bmcs_matmod/ms1/__init__.py
@@ -1,7 +1,5 @@
 from .ms1 import MS1
 from .ms1 import MS13D
-from .ms1_damage_jirasek import MS1_damage_jir
-from .ms1_damage_jirasek import MS13D_damage_jir
 from .ms1_sigma_pvw import MS1PVW
 from .ms1_sigma_pvw import MS13DPVW
 from .ms1_eeq_explicit import MS1EEQ
