2c3a6bb42398b17a70f81b3ffc86f3fe5d0ad719
marag <mario.aguilar@rwth-aachen.de>
2022-02-17 11:47:59 +0100

    update on work analysis of microplane homogenization approaches

--- bmcs_matmod/ms1/__init__.py
+++ bmcs_matmod/ms1/__init__.py
@@ -3,5 +3,6 @@ from .ms1 import MS13D
 from .ms1_damage_jirasek import MS1_damage_jir
 from .ms1_damage_jirasek import MS13D_damage_jir
 from .ms1_sigma_pvw import MS1PVW
-from .ms1_sigma_pvw import MS13D_sigma_int
-
+from .ms1_sigma_pvw import MS13DPVW
+from .ms1_eeq_explicit import MS1EEQ
+from .ms1_eeq_explicit import MS13DEEQ
