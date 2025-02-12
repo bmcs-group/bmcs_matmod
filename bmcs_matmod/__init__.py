e63e6104845386e91fa3bbe59ff45b156eaca141
rch <rostislav.chudoba@rwth-aachen.de>
2025-02-12 10:35:11 +0100

    setup of environment using requirements.txt

--- bmcs_matmod/__init__.py
+++ bmcs_matmod/__init__.py
@@ -1,9 +1,3 @@
 
 from bmcs_matmod.version import __version__
-# from .ms1 import MS1
-# from .ms1 import MS13D
-from .msx import MSX
-from .gsm import GSMMPDP
-from .gsm import GSMRM
-from .gsm import GSMNR
 
