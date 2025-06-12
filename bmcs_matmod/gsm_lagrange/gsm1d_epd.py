685720537fbf9a782e54c8234fab4e9887a41488
rch <rostislav.chudoba@rwth-aachen.de>
2025-06-12 20:12:58 +0200

    further updates for epd and vepd

--- bmcs_matmod/gsm_lagrange/gsm1d_epd.py
+++ bmcs_matmod/gsm_lagrange/gsm1d_epd.py
@@ -8,12 +8,14 @@ class GSM1D_EPD(GSMDef):
 
     # ## Material parameters
     E = Scalar(r'E', real=True, nonnegative=True, codename='E')
+    K = Scalar(r'K', real=True, positive=True, codename='K')
+    gamma = Scalar(r'\gamma', real=True, positive=True, codename='gamma')
     S = Scalar(r'S', real=True, nonnegative=True, codename='S')
     c = Scalar(r'c', real=True, nonnegative=True, codename='c')
     r = Scalar(r'r', real=True, nonnegative=True, codename='r')
-    eps_0 = Scalar(r'\varepsilon_0', real=True, nonnegative=True, codename='eps_0')
+    f_c = Scalar(r'f_\mathrm{c}', positive=True, real=True, codename='f_c')
 
-    mparams = (E, S, c, r, eps_0)
+    mparams = (E, K, gamma, S, c, r, f_c)
 
     # ## External state variables
     eps = Scalar(r'\varepsilon', real=True, codename='eps')
@@ -32,17 +34,29 @@ class GSM1D_EPD(GSMDef):
     sig_p = Scalar(r'\sigma^\mathrm{p}', real=True, codename='sig_p')
     sig_p_a = Vector(r'\sigma^\mathrm{p}_{a}', [sig_p], codename='sig_p_a')
 
+    z = Scalar(r'z', real=True, nonnegative=True, codename='z')
+    z_a = Vector(r'z_{a}', [z], codename='z_a')
+    Z = Scalar(r'Z', real=True, nonnegative=True, codename='Z')
+    Z_a = Vector(r'Z_{a}', [Z], codename='Z_a')
+
+    alpha = Scalar(r'\alpha', real=True, nonnegative=True, codename='alpha')
+    alpha_a = Vector(r'\alpha_{a}', [alpha], codename='alpha_a')
+    X = Scalar(r'X', real=True, nonnegative=True, codename='X')
+    X_a = Vector(r'X_{a}', [X], codename='X_a')
+
     # ## Free energy potential
     eps_el = eps - eps_p
-    F_ = sp.Rational(1,2) * (1 - omega) * E * eps_el**2
+    U_e_ = sp.Rational(1,2) * (1 - omega) * E * eps_el**2
+    U_p_ = sp.Rational(1,2) * K * z**2 + sp.Rational(1,2) * gamma * alpha**2
+    F_ = U_e_ + U_p_
 
     # ## Flow potential
-    f_ = sp.Abs(sig_p) - (eps_0 + S)
+    f_ = sp.sqrt((sig_p/(1-omega) - X)**2) - (f_c + Z)
     phi_ext_ = (1 - omega)**c * (S/(r+1)) * (Y / S)**(r+1)
 
-    Eps_vars = (omega_a, eps_p_a)
-    Sig_vars = (Y_a, sig_p_a)
-    Sig_signs = (-1, -1)
+    Eps_vars = (eps_p_a, omega_a, z_a, alpha_a)
+    Sig_vars = (sig_p_a, Y_a, Z_a, X_a)
+    Sig_signs =  (-1, -1, 1, 1)
 
     F_engine = GSMEngine(
         name = 'gsm_F_1d_mpdp_epd',
